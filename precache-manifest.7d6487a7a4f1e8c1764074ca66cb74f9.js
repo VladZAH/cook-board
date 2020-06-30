54517d85d2e45d1a05051a1e94fdeb46b1236816
Volodymyr Zahorodnyy <volodymyr@Volodymyrs-MBP.hitronhub.home>
2020-06-30 14:32:01 -0700

    Updates

---
+++ precache-manifest.7d6487a7a4f1e8c1764074ca66cb74f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c66d235350a098c8389cbf66ce1c1f",
+    "url": "/.github.io/cook-board/index.html"
+  },
+  {
+    "revision": "8672b0a08925c0e59370",
+    "url": "/.github.io/cook-board/static/css/main.99cbe4a1.chunk.css"
+  },
+  {
+    "revision": "7d0d52804b8c39bbb379",
+    "url": "/.github.io/cook-board/static/js/2.cf1751fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/cook-board/static/js/2.cf1751fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8672b0a08925c0e59370",
+    "url": "/.github.io/cook-board/static/js/main.8dfab047.chunk.js"
+  },
+  {
+    "revision": "85e9505f04922c87672a",
+    "url": "/.github.io/cook-board/static/js/runtime-main.904a6447.js"
+  },
+  {
+    "revision": "a985bca38d11a68af42acb2a3a315bbd",
+    "url": "/.github.io/cook-board/static/media/CookBoardLogo.a985bca3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/.github.io/cook-board/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a957eec2dc9eba4e7ff12a0d53bf616e",
+    "url": "/.github.io/cook-board/static/media/no-image.a957eec2.png"
+  }
+]);
